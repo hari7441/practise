07df3564a421db0d8c02b03fa7d9522ec8f97716
hari-7441 <kothakoti.harikrishna@gmail.com>
2025-07-25 03:21:33 +0200

    first commit

--- 02-arrays.sh
+++ 02-arrays.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-Fruits = ("Apple" "Banana" "Cherry" "Date" "Elderberry")
+Fruits=("Apple" "Banana" "Cherry" "Date" "Elderberry")
 echo "The first fruit is: ${Fruits[0]}"
 echo "The second fruit is: ${Fruits[1]}"
 echo "The third fruit is: ${Fruits[2]}"
